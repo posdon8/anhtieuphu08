083f90d38f140bcb7b8d58ad180a5971ddc7abb5
posdon8 <137530351+posdon8@users.noreply.github.com>
2023-06-25 21:47:52 +0700

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, s, p;
+            Console.WriteLine("nhap chieu dai a: ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("nhap chieu rong b: ");
+            b = float.Parse(Console.ReadLine());
+            s = a * b;
+            p = (a + b) * 2;
+            Console.WriteLine("dien tich la {0}, chu vi la {1}", s, p);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float a,b,x,y;
+            Console.WriteLine("nhap vao tong a= ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("nhap vao hieu b= ");
+            b = float.Parse(Console.ReadLine());
+            x = (a + b)/2;
+            y = (a - b) / 2;
+            Console.WriteLine(" gia tri x la " + x);
+            Console.WriteLine(" gia tri y la " + y);
+            Console.ReadKey();
+        }
+    }
+}
